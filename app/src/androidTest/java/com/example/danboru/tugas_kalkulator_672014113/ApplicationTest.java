cdeaf47d6c2247bd7b1b4c5d630665e9d40d5c8f
Danboru <8danang8@gmail.com>
2016-05-18 10:42:11 +0700

    Desain awal

---
+++ app/src/androidTest/java/com/example/danboru/tugas_kalkulator_672014113/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.danboru.tugas_kalkulator_672014113;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
