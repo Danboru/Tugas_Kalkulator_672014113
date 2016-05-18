225f55158dde1743aef101f0f7b41e7b8f2154b7
Danboru <8danang8@gmail.com>
2016-05-18 12:03:26 +0700

    Hapus 2 Toast

--- app/src/main/java/com/example/danboru/tugas_kalkulator_672014113/MainActivity.java
+++ app/src/main/java/com/example/danboru/tugas_kalkulator_672014113/MainActivity.java
@@ -70,14 +70,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.tiga : {
 
-                Toast.makeText(MainActivity.this, "Hello :)" , Toast.LENGTH_SHORT).show();
 
                 break;
             }
 
             case R.id.empat : {
 
-                Toast.makeText(MainActivity.this, "Hello :)" , Toast.LENGTH_SHORT).show();
 
                 break;
             }
